4191fde44d6b234235d29918af08663e95f639c6
Philipp Waldinger <philipp.waldinger@campus.lmu.de>
2021-12-29 20:54:15 +0100

    final draft categories plot

--- diagrams/categories.py
+++ diagrams/categories.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def draw(data):
     plt.rc('text', usetex=True)
+    plt.rcParams.update({'font.size': 11})
 
     colors = [
         (27/255, 153/255, 139/255),
@@ -31,7 +32,7 @@ def draw(data):
     error = list(map(lambda key: data[key][Category.error], data))
     wrong = list(map(lambda key: data[key][Category.wrong], data))
 
-    fig, ax = plt.subplots(figsize=(13,4))
+    fig, ax = plt.subplots(figsize=(13,5))
     bars_correct = ax.bar(x - 1 * width, correct, width, label='Correct', color=colors[0])
     bars_error = ax.bar(x, error, width, label='Error', color=colors[1])
     bars_wrong = ax.bar(x + 1 * width, wrong, width, label='Wrong', color=colors[2])
@@ -40,11 +41,13 @@ def draw(data):
         'Number of Corresponding Results',
         rotation=0
     )
-    ax.yaxis.set_label_coords(0.047, 1.06)
+    ax.yaxis.set_label_coords(0.05, 1.06)
 
     ax.legend(
         frameon=False,
-        bbox_to_anchor=(1.11, 0.6)
+        bbox_to_anchor=(1.0, 1.01),
+        loc='lower right'
+        # bbox_to_anchor=(1.11, 0.6)
     )
 
     ax.tick_params(axis='x', pad=7)
